f40bf80b57dcbd85f7f48fdc14cffa4fe4d1d9bc
Daan-25 <180635521+Daan-25@users.noreply.github.com>
2025-09-26 22:44:09 +0200

    Add sample data to comments table

--- migrations/0001_create_comments_table.sql
+++ migrations/0001_create_comments_table.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS comments (
 -- Insert some sample data into our comments table.
 INSERT INTO comments (author, content)
 VALUES
+    ('Daan', 'Hi!'),
     ('Kristian', 'Congrats!'),
     ('Serena', 'Great job!'),
     ('Max', 'Keep up the good work!')
